0b179fbe35486d49b2ca40df564c59cc8a74533d
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2023-03-29 20:58:30 +0100

    progress

---
+++ Assets/Jon F/Scripts/ReceptacleManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceptacleManager : MonoBehaviour
+{
+    public uint sumB;
+    public uint sumY;
+    public uint sumR;
+    public Text sumTxtB;
+    public Text sumTxtY;
+    public Text sumTxtR;
+
+    public List<int> plantsReceived = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
