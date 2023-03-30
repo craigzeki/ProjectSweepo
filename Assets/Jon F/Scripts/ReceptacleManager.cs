8422acda6eeafdfa8f7d459831fb2c7f04952cb6
JonFieldhouse93 <75310581+JonFieldhouse93@users.noreply.github.com>
2023-03-30 20:52:10 +0100

    receptacle script/tags for plants/updated prefabs

--- Assets/Jon F/Scripts/ReceptacleManager.cs
+++ Assets/Jon F/Scripts/ReceptacleManager.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ReceptacleManager : MonoBehaviour
 {
     public uint sumB;
     public uint sumY;
     public uint sumR;
-    public Text sumTxtB;
-    public Text sumTxtY;
-    public Text sumTxtR;
+    public TextMeshProUGUI sumTxtB;
+    public TextMeshProUGUI sumTxtY;
+    public TextMeshProUGUI sumTxtR;
 
-    public List<int> plantsReceived = new List<int>();
+    //public List<int> plantsReceived = new List<int>();
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,30 @@ public class ReceptacleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        sumTxtB.text = ((uint)sumB).ToString();
+        sumTxtR.text = ((uint)sumR).ToString();
+        sumTxtY.text = ((uint)sumY).ToString();
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "blueplant")
+        {
+            sumB++;
+            Debug.Log("Collision detected");
+        }
+        if (collision.gameObject.tag == "redplant")
+        {
+            sumR++; Debug.Log("Collision detected");
+
+        }
+        if (collision.gameObject.tag == "yellowplant")
+        {
+            sumY++;
+            Debug.Log("Collision detected");
+
+        }
+        else return;
+    }
+
 }
